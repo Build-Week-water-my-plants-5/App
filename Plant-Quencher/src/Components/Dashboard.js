9256261a7bcc482bfeab17d550cf4d4732cf0267
Vrndavana <Vrmdavama.th@gmail.com>
2020-03-04 17:05:30 -0800

    Successful API hit using Test pass: CSS: Plant Image

---
+++ Plant-Quencher/src/Components/Dashboard.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Dashboard = () => {
+
+        
+
+
+
+    return (
+        <div className="Dash"> 
+        
+        <h1>Welcome to your Dashboard. </h1>
+
+
+
+
+
+
+
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+export default Dashboard;
+
+
+
+//   export default connect(
+//     mapStateToProps,
+//     { getPosts, addPost }          
+//   )(Dashboard);
+
--- Plant-Quencher/src/Components/SignUp.js
+++ Plant-Quencher/src/Components/SignUp.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
+
+
+
 const UserForm = ({ values, errors, touched, status }) => {
   const [users, setUsers] = useState([]);
   useEffect(() => {
